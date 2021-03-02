972d55d6a2a8c36b74ffa011d931b7901093281e
JX <jianxiong@mycontinuum.health>
2021-03-02 16:20:11 +0000

    first commit

---
+++ src/main/java/com/correvate/zip/RemoteZipperApplication.java
@@ -0,0 +1,13 @@
+package com.correvate.zip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteZipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteZipperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/correvate/zip/RemoteZipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correvate.zip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteZipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
